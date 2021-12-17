ce7817dbd2533eed748ba07a5a88dba8be084094
eronth <github@nicpereira.dev>
2021-12-17 15:31:40 -0600

    init rename commit

---
+++ module/bunkers-and-badasses.mjs
@@ -0,0 +1,127 @@
+// Import document classes.
+import { BNBActor } from "./documents/actor.mjs";
+import { BNBItem } from "./documents/item.mjs";
+// Import sheet classes.
+import { BNBActorSheet } from "./sheets/actor-sheet.mjs";
+import { BNBItemSheet } from "./sheets/item-sheet.mjs";
+// Import helper/utility classes and constants.
+import { preloadHandlebarsTemplates } from "./helpers/templates.mjs";
+import { BNB } from "./helpers/config.mjs";
+
+/* -------------------------------------------- */
+/*  Init Hook                                   */
+/* -------------------------------------------- */
+
+Hooks.once('init', async function() {
+
+  // Add utility classes to the global game object so that they're more easily
+  // accessible in global contexts.
+  game.bnb = {
+    BNBActor: BNBActor,
+    BNBItem: BNBItem,
+    rollItemMacro
+  };
+
+  // Add custom constants for configuration.
+  CONFIG.BNB = BNB;
+
+  /**
+   * Set an initiative formula for the system
+   * @type {String}
+   */
+  CONFIG.Combat.initiative = {
+    formula: "1d20 + @abilities.dex.mod",
+    decimals: 2
+  };
+
+  // Define custom Document classes
+  CONFIG.Actor.documentClass = BNBActor;
+  CONFIG.Item.documentClass = BNBItem;
+
+  // Register sheet application classes
+  Actors.unregisterSheet("core", ActorSheet);
+  Actors.registerSheet("bunkers-and-badasses", BNBActorSheet, { makeDefault: true });
+  Items.unregisterSheet("core", ItemSheet);
+  Items.registerSheet("bunkers-and-badasses", BNBItemSheet, { makeDefault: true });
+
+  // Preload Handlebars templates.
+  return preloadHandlebarsTemplates();
+});
+
+/* -------------------------------------------- */
+/*  Handlebars Helpers                          */
+/* -------------------------------------------- */
+
+// If you need to add Handlebars helpers, here are a few useful examples:
+Handlebars.registerHelper('concat', function() {
+  var outStr = '';
+  for (var arg in arguments) {
+    if (typeof arguments[arg] != 'object') {
+      outStr += arguments[arg];
+    }
+  }
+  return outStr;
+});
+
+Handlebars.registerHelper('toLowerCase', function(str) {
+  return str.toLowerCase();
+});
+
+/* -------------------------------------------- */
+/*  Ready Hook                                  */
+/* -------------------------------------------- */
+
+Hooks.once("ready", async function() {
+  // Wait to register hotbar drop hook on ready so that modules could register earlier if they want to
+  Hooks.on("hotbarDrop", (bar, data, slot) => createItemMacro(data, slot));
+});
+
+/* -------------------------------------------- */
+/*  Hotbar Macros                               */
+/* -------------------------------------------- */
+
+/**
+ * Create a Macro from an Item drop.
+ * Get an existing item macro if one exists, otherwise create a new one.
+ * @param {Object} data     The dropped data
+ * @param {number} slot     The hotbar slot to use
+ * @returns {Promise}
+ */
+async function createItemMacro(data, slot) {
+  if (data.type !== "Item") return;
+  if (!("data" in data)) return ui.notifications.warn("You can only create macro buttons for owned Items");
+  const item = data.data;
+
+  // Create the macro command
+  const command = `game.bnb.rollItemMacro("${item.name}");`;
+  let macro = game.macros.entities.find(m => (m.name === item.name) && (m.command === command));
+  if (!macro) {
+    macro = await Macro.create({
+      name: item.name,
+      type: "script",
+      img: item.img,
+      command: command,
+      flags: { "bnb.itemMacro": true }
+    });
+  }
+  game.user.assignHotbarMacro(macro, slot);
+  return false;
+}
+
+/**
+ * Create a Macro from an Item drop.
+ * Get an existing item macro if one exists, otherwise create a new one.
+ * @param {string} itemName
+ * @return {Promise}
+ */
+function rollItemMacro(itemName) {
+  const speaker = ChatMessage.getSpeaker();
+  let actor;
+  if (speaker.token) actor = game.actors.tokens[speaker.token];
+  if (!actor) actor = game.actors.get(speaker.actor);
+  const item = actor ? actor.items.find(i => i.name === itemName) : null;
+  if (!item) return ui.notifications.warn(`Your controlled Actor does not have an item named ${itemName}`);
+
+  // Trigger the item roll
+  return item.roll();
+}
---
+++ module/helpers/config.mjs
@@ -0,0 +1,23 @@
+export const BNB = {};
+
+/**
+ * The set of Ability Scores used within the sytem.
+ * @type {Object}
+ */
+ BNB.abilities = {
+  "str": "BNB.AbilityStr",
+  "dex": "BNB.AbilityDex",
+  "con": "BNB.AbilityCon",
+  "int": "BNB.AbilityInt",
+  "wis": "BNB.AbilityWis",
+  "cha": "BNB.AbilityCha"
+};
+
+BNB.abilityAbbreviations = {
+  "str": "BNB.AbilityStrAbbr",
+  "dex": "BNB.AbilityDexAbbr",
+  "con": "BNB.AbilityConAbbr",
+  "int": "BNB.AbilityIntAbbr",
+  "wis": "BNB.AbilityWisAbbr",
+  "cha": "BNB.AbilityChaAbbr"
+};
