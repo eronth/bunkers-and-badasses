66eecc489ec784a9e7d041f58708ad86209a88b7
eronth <github@nicpereira.dev>
2022-06-07 02:05:50 -0500

    Restructure builder tab folders.

--- module/helpers/templates.mjs
+++ module/helpers/templates.mjs
@@ -12,34 +12,29 @@
     "systems/bunkers-and-badasses/templates/actor/parts/vh/header/healths-block.html",
     "systems/bunkers-and-badasses/templates/actor/parts/vh/header/stats-block.html",
     "systems/bunkers-and-badasses/templates/actor/parts/npc/header/stats.html",
-    
-    /// Body Components
+    /// Body tabs
     "systems/bunkers-and-badasses/templates/actor/parts/vh/action-tab.html",
     "systems/bunkers-and-badasses/templates/actor/parts/npc/action-tab.html",
     "systems/bunkers-and-badasses/templates/actor/parts/loot-tab.html",
     "systems/bunkers-and-badasses/templates/actor/parts/vh/builder-tab.html",
     "systems/bunkers-and-badasses/templates/actor/parts/bio-tab.html",
     "systems/bunkers-and-badasses/templates/actor/parts/effects-tab.html",
-    
-    /// Action components
+    /// Action tab components
     "systems/bunkers-and-badasses/templates/actor/parts/vh/action-components/checks-block.html",
     "systems/bunkers-and-badasses/templates/actor/parts/vh/action-components/check.html",
     "systems/bunkers-and-badasses/templates/actor/parts/vh/action-components/hp-display-block.html",
     "systems/bunkers-and-badasses/templates/actor/parts/npc/action-components/action-block.html",
-    /// Builder components
+    /// Builder subtabs
+    "systems/bunkers-and-badasses/templates/actor/parts/vh/builder-subtabs/level-up-subtab.html",
+    "systems/bunkers-and-badasses/templates/actor/parts/vh/builder-subtabs/archetypes-subtab.html",
+    "systems/bunkers-and-badasses/templates/actor/parts/vh/builder-subtabs/class-subtab.html",
+    "systems/bunkers-and-badasses/templates/actor/parts/vh/builder-subtabs/class-skills-subtab.html",
+    /// Builder subtab components
+    "systems/bunkers-and-badasses/templates/actor/parts/vh/builder-subtabs/builder-components/favored-element-component.html",
+    "systems/bunkers-and-badasses/templates/actor/parts/vh/builder-subtabs/builder-components/archetype-component.html",
     
-    /// Old (to be moved)
+    /// Old (to be moved or removed)
     "systems/bunkers-and-badasses/templates/actor/parts/actor-features.html",
-
-
-
-
-    "systems/bunkers-and-badasses/templates/actor/parts/builder-components/actor-builder-levelup.html",
-    "systems/bunkers-and-badasses/templates/actor/parts/builder-components/favored-element-component.html",
-    "systems/bunkers-and-badasses/templates/actor/parts/builder-components/actor-builder-archetypes.html",
-    "systems/bunkers-and-badasses/templates/actor/parts/builder-components/actor-builder-individual-archetype.html",
-    "systems/bunkers-and-badasses/templates/actor/parts/builder-components/actor-builder-class.html",
-    "systems/bunkers-and-badasses/templates/actor/parts/builder-components/actor-builder-class-skills.html",
 
     // Item partials
     "systems/bunkers-and-badasses/templates/item/parts/rarity-dropdown.html",
@@ -50,6 +45,7 @@
     "systems/bunkers-and-badasses/templates/item/parts/health-type-dropdown.html",
     "systems/bunkers-and-badasses/templates/item/parts/red-text.html",
 
+    // Dialog partials
     "systems/bunkers-and-badasses/templates/dialog/parts/element-radio-button.html"
   ]);
 };
