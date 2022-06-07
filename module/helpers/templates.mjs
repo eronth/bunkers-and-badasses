922c8607efbc3cd760e147eac7ff15f2dd852b18
eronth <github@nicpereira.dev>
2022-06-07 01:20:44 -0500

    Fix health displays cutting off numbers (#34)
    * Breakout css a bit.
    
    * Font size fix.
    
    * Set up a header css file
    
    * Header health displays better now.
    
    * Better hp block sizing.
    
    * Indentation
    
    * More cleanup because I can.

--- module/helpers/templates.mjs
+++ module/helpers/templates.mjs
@@ -6,8 +6,15 @@
  export const preloadHandlebarsTemplates = async function() {
   return loadTemplates([
 
-    // Actor partials.
-    "systems/bunkers-and-badasses/templates/actor/parts/actor-experience-bar.html",
+    // Actor partials
+    /// Header Components
+    "systems/bunkers-and-badasses/templates/actor/parts/vh/header/experience-bar.html",
+    "systems/bunkers-and-badasses/templates/actor/parts/vh/header/healths-block.html",
+    "systems/bunkers-and-badasses/templates/actor/parts/vh/header/stats-block.html",
+    
+    
+    
+    /// Old (to be moved)
     "systems/bunkers-and-badasses/templates/actor/parts/actor-bio.html",
     "systems/bunkers-and-badasses/templates/actor/parts/actor-features.html",
     "systems/bunkers-and-badasses/templates/actor/parts/actor-loot.html",
@@ -31,6 +38,7 @@
     "systems/bunkers-and-badasses/templates/actor/parts/builder-components/actor-builder-class.html",
     "systems/bunkers-and-badasses/templates/actor/parts/builder-components/actor-builder-class-skills.html",
 
+    // Item partials
     "systems/bunkers-and-badasses/templates/item/parts/rarity-dropdown.html",
     "systems/bunkers-and-badasses/templates/item/parts/element-selector.html",
     "systems/bunkers-and-badasses/templates/item/parts/damage-entry.html",
