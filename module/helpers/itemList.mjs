0b8b8d80b35b5537958e8ab8c9a51d22cdc02045
eronth <github@nicpereira.dev>
2023-12-01 17:34:47 -0600

    Empty templates added.

--- module/helpers/itemList.mjs
+++ module/helpers/itemList.mjs
@@ -1,10 +1,21 @@
 export class ItemList {
   static getItemDetailsBlockTemplateLocation(detailsTemplateType) {
-    switch (detailsTemplateType) {
-      case 'archetypeFeat':
+    switch ((detailsTemplateType ?? '').toLowerCase()) {
+      case 'grenade mod':
+      case 'grenademod':
+        return 'systems/bunkers-and-badasses/templates/actor/parts/item-list-components/item-detail/grenade-mod-item-detail-component.html';
+
+      case 'archetype feat':
+      case 'archetypefeat':
         return 'systems/bunkers-and-badasses/templates/actor/parts/item-list-components/item-detail/archetype-feat-item-detail-component.html';
       default:
         return 'systems/bunkers-and-badasses/templates/actor/parts/item-list-components/item-detail/default-item-detail-component.html';
     }
   }
 }
+
+//     "systems/bunkers-and-badasses/templates/actor/parts/item-list-components/item-detail/gun-item-detail-component.html",
+//     "systems/bunkers-and-badasses/templates/actor/parts/item-list-components/item-detail/key-item-detail-component.html",
+//     "systems/bunkers-and-badasses/templates/actor/parts/item-list-components/item-detail/potion-item-detail-component.html",
+//     "systems/bunkers-and-badasses/templates/actor/parts/item-list-components/item-detail/relic-item-detail-component.html",
+//     "systems/bunkers-and-badasses/templates/actor/parts/item-list-components/item-detail/shield-item-detail-component.html",
